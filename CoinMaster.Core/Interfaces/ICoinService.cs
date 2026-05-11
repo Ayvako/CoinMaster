a6c90bd8e6c8c980b1e6cb961b544ce3735972cd
Ayvako <ayvako2022@gmail.com>
2026-05-11 03:31:48 +0300

    Added convert

--- CoinMaster.Core/Interfaces/ICoinService.cs
+++ CoinMaster.Core/Interfaces/ICoinService.cs
@@ -7,4 +7,6 @@ public interface ICoinService
     Task<List<Coin>> GetTopCoinsAsync(int limit = 10);
 
     Task<Coin> GetDetailsAsync(string id, string days = "7");
+
+    Task<decimal> ConvertAsync(string fromCoinId, string toCoinId, decimal amount);
 }
--- CoinMaster.Core/Services/CoinService.cs
+++ CoinMaster.Core/Services/CoinService.cs
@@ -5,22 +5,41 @@ namespace CoinMaster.Core.Services;
 
 public class CoinService : ICoinService
 {
-    private readonly ICoinProvider client;
+    private readonly ICoinProvider coinProvider;
 
     private readonly IMarketProvider marketProvider;
 
     private readonly IChartProvider chartProvider;
 
-    public CoinService(ICoinProvider client, IMarketProvider marketProvider, IChartProvider chartProvider)
+    public CoinService(ICoinProvider coinProvider, IMarketProvider marketProvider, IChartProvider chartProvider)
     {
-        this.client = client;
+        this.coinProvider = coinProvider;
         this.marketProvider = marketProvider;
         this.chartProvider = chartProvider;
     }
 
+    public async Task<decimal> ConvertAsync(string fromCoinId, string toCoinId, decimal amount)
+    {
+        var fromTask = coinProvider.GetDetailsAsync(fromCoinId);
+        var toTask = coinProvider.GetDetailsAsync(toCoinId);
+
+        await Task.WhenAll(fromTask, toTask);
+
+        var fromCoin = fromTask.Result;
+        var toCoin = toTask.Result;
+
+        if (fromCoin == null || toCoin == null)
+            throw new InvalidOperationException($"Coin not found");
+
+        if (toCoin.PriceUsd == 0)
+            throw new InvalidOperationException($"{toCoinId} has zero price");
+
+        return amount * (fromCoin.PriceUsd / toCoin.PriceUsd);
+    }
+
     public async Task<Coin> GetDetailsAsync(string id, string days = "7")
     {
-        var coinTask = client.GetDetailsAsync(id);
+        var coinTask = coinProvider.GetDetailsAsync(id);
         var marketsTask = marketProvider.GetMarketsAsync(id);
         var ohlcTask = chartProvider.GetOhlcAsync(id, days);
 
@@ -33,5 +52,5 @@ public class CoinService : ICoinService
     }
 
     public Task<List<Coin>> GetTopCoinsAsync(int limit = 10)
-        => client.GetTopCoinsAsync(limit);
+        => coinProvider.GetTopCoinsAsync(limit);
 }
