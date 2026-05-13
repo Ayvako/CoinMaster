579f569755fe509d340d52b5ac3184484839f381
Ayvako <ayvako2022@gmail.com>
2026-05-13 17:02:59 +0300

    Show placeholder UI when Candles or Markets list is empty.

---
+++ CoinMaster.Client/Converters/EmptyToVisibleConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CoinMaster.Client.Converters;
+
+[ValueConversion(typeof(IEnumerable), typeof(Visibility))]
+public class EmptyToVisibleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        bool isEmpty = value is not ICollection { Count: > 0 };
+        bool invert = parameter is string s && s == "invert";
+        return (isEmpty ^ invert) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
